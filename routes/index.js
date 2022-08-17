8b43a3fd1efff8b0931f6d5639f508d08928f6d7
joyceyyuan <yueyuan@hotmail.com>
2022-08-17 11:22:10 -0700

    set new homepage

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.redirect('/trails');
+  res.render('index.ejs');
 });
 
 // Google OAuth login route
@@ -18,14 +18,14 @@ router.get('/oauth2callback', passport.authenticate(
   'google',
   {
     successRedirect : '/trails', // UPDATE THIS, where do you want the client to go after you login 
-    failureRedirect : '/trails' //  UPDATE THIS, where do you want the client to go if login fails
+    failureRedirect : '/' //  UPDATE THIS, where do you want the client to go if login fails
   }
 ));
 
 // OAuth logout route
 router.get('/logout', function(req, res){
   req.logout(function(){ //< - req.logout comes from passport, and what it does is destorys the cookie keeping track of the user!
-    res.redirect('/trails') // <---- UPDATE THIS TO WHERE YOU WANT THE USER TO GO AFTER LOGOUT
+    res.redirect('/') // <---- UPDATE THIS TO WHERE YOU WANT THE USER TO GO AFTER LOGOUT
   })
 })
 
--- routes/trails.js
+++ routes/trails.js
@@ -5,11 +5,11 @@ const isLoggedIn = require('../config/auth')
 /* GET users listing. */
 router.get('/', trailController.index);
 router.get('/new', isLoggedIn, trailController.new);
-router.get('/mytrails', isLoggedIn, trailController.new);
+// router.get('/mytrails', isLoggedIn, trailController.new);
 router.get('/:id', trailController.show);
 router.post('/', isLoggedIn, trailController.create);
 router.get('/:id/details', trailController.edit);
-// router.delete('/:id', trailController.delete);
-// router.put('/:id', trailController.update);
+router.delete('/:id', trailController.delete);
+router.put('/:id', trailController.update);
 
 module.exports = router;
